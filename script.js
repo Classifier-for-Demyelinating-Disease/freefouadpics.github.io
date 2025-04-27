8f4d303a977555b79baa6c9b08065e95318a9875
ozzux <osama.radi7@gmail.com>
2025-04-27 14:41:17 +0300

    added spin

---
+++ script.js
@@ -0,0 +1,29 @@
+spinner(document.getElementById('pic1'))
+spinner(document.getElementById('pic2'))
+spinner(document.getElementById('pic3'))
+spinner(document.getElementById('pic4'))
+spinner(document.getElementById('pic5'))
+spinner(document.getElementById('pic6'))
+spinner(document.getElementById('pic7'))
+spinner(document.getElementById('pic8'))
+spinner(document.getElementById('pic9'))
+spinner(document.getElementById('pic10'))
+spinner(document.getElementById('pic11'))
+spinner(document.getElementById('pic12'))
+
+
+
+function spinner(pic){
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function spin(picture) {
+        for (let i = 0; i <= 360; i++){
+            picture.style.rotate = `${i}deg`;
+            await sleep(1);
+        }
+    }
+
+    pic.onpointerdown = () => spin(pic);
+}
